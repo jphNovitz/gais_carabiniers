2610f5cb302363edd3d1aa68c9d7d56d53755a0a
Novitz Jean-Philippe <novitz@gmail.com>
2021-09-28 22:18:31 +0200

    email verifyer stof encore sass loader

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
---
+++ readme.md
@@ -0,0 +1,7 @@
+Symfony Boilerplate  
+  
+Symfony 5.4  
+- stof doctrine extension  
+- symfonycasts/verify-email-bundle
+- symfony/webpack-encore-bundle
+--  sass-loader@^7.0.1 sass --dev

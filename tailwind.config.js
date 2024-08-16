7d2b6e3a3da2ddc3417927b1354256fffd1a050d
Novitz Jean-Philippe <novitz@gmail.com>
2024-08-16 22:03:56 +0200

    Update - symfony 7.1 - add colors neutral monochrome

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'spacing': 'margin, padding, display',
+        'width': 'width',
+      },
+      colors: {
+        "transparent": "transparent",
+        "white": "#F8F9FA",
+        "black": "#212529",
+        "light-text-base": "#212529",
+        "light-text-secondary": "#343A40",
+        "dark-text-base": "#212529",
+        "dark-text-secondary": "#343A40",
+        "light-primary": "#ADB5BD",
+        "dark-primary": "#ADB5BD",
+        "light-neutral": "#F8F9FA",
+        "dark-neutral": "#F8F9FA",
+      },
+    },
   },
   plugins: [],
 }

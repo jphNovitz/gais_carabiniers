0ee265a09fa11cd620059a434e246ce04371bb80
Novitz Jean-Philippe <novitz@gmail.com>
2024-11-17 23:43:17 +0100

    Add composer.json, tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-  ],
-  theme: {
-    extend: {
-      transitionProperty: {
-        'spacing': 'margin, padding, display',
-        'width': 'width',
-      },
-      colors: {
-        "transparent": "transparent",
-        "white": "#F8F9FA",
-        "base": {
-          "light": "#FFFFFF", // Blanc pur pour le fond en mode clair
-          "dark": "#121212",  // Noir profond pour le fond en mode sombre
+    content: [
+        "./assets/**/*.js",
+        "./templates/**/*.html.twig",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'footer': "url('/images/logo-gais-carabiniers-bernissart.webp')",
+            },
+            transitionProperty: {
+                'spacing': 'margin, padding, display',
+                'width': 'width',
+            },
+            transitionDuration: {
+                '2000': '2000ms',
+                '5000': '5000ms',
+            },
+            colors: {
+                "transparent": "transparent",
+                "white": "#F8F9FA",
+                "base": {
+                    "light": "#EDE1B8", // Beige légèrement plus clair pour la base
+                    "dark": "#1B1B1B",  // Proche du noir pour une teinte de base sobre en mode sombre
+                },
+                "surface": {
+                    "light": "#eee9db", // Beige plus prononcé pour les sections et cartes en mode clair
+                    "dark": "#3E3E3E",  // Une teinte foncée pour le mode sombre
+                    "secondary": "#A53A1A", // Rouge atténué pour un contraste esthétique
+                },
+                "content": {
+                    "primary": {
+                        "light": "#1B1B1B",  // Vert pour les titres et textes principaux
+                        "dark": "#eee9db",   // Version plus sombre du vert pour le mode sombre
+                        // "light": "#3B7C45",  // Vert pour les titres et textes principaux
+                        // "dark": "#2A5A30",   // Version plus sombre du vert pour le mode sombre
+                    },
+                    "secondary": "#3B7C45", // Jaune vif pour les éléments de mise en avant
+                    // "secondary": "#F2C029", // Jaune vif pour les éléments de mise en avant
+                },
+                "secondary": "#3B7C45", // Jaune vif pour les éléments de mise en avant
+
+            },
         },
-        "surface": {
-          "light": "#F5F5F5",
-          "dark": "#1E1E1E",
-          "secondary": "#B0B0B0",
-        },
-        "content": {
-          "primary": {
-            "light": "#333333",
-            "dark": "#E0E0E0",
-          },
-          "secondary": "#8C8C8C",
-        },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 }

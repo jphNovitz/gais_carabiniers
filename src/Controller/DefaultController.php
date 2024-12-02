1b7aa8000e0c00bdf173d1ed80b085af22555178
Novitz Jean-Philippe <novitz@gmail.com>
2024-12-03 00:54:27 +0100

    Refactor DefaultController

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Repository\ClubRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\UX\Map\Map;
 use Symfony\UX\Map\Point;
 use Symfony\UX\Map\Marker;

9452dcc0c433b4d7e6fcd6f3c96be046e2e41fe8
Novitz Jean-Philippe <novitz@gmail.com>
2024-12-04 00:57:21 +0100

    Add contact route

--- src/Controller/Club/ClubController.php
+++ src/Controller/Club/ClubController.php
@@ -14,11 +14,55 @@ use Symfony\UX\Map\InfoWindow;
 class ClubController extends AbstractController
 {
     #[Route('/notre-histoire', name: 'app_about')]
-    public function index(ClubRepository $clubRepository): Response
+    public function about(): Response
     {
-
         return $this->render('club/about.html.twig', [
-//            $clubs = $clubRepository->findAll()[0],
+        ]);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(ClubRepository $clubRepository): Response
+    {
+        $myMap = (new Map());
+        $myMap
+            // Explicitly set the center and zoom
+            ->center(new Point(50.472738, 3.661204))
+//            ->zoom(6)
+
+            // Or automatically fit the bounds to the markers
+            ->fitBoundsToMarkers()
+            ->addMarker(new Marker(
+                position: new Point(50.472738, 3.661204),
+//                title: 'Les Gais Carabiniers de Bernissart',
+                infoWindow: new InfoWindow(
+                    headerContent: '<h3>Les Gais Carabiniers de Bernissart</h3>',
+                    content: '<p>Rue Lotard 16, 7320 Bernissart</p>'
+                ),
+
+//                icon: 'images/logo-gais-carabiniers-bernissart.webp'
+            ))
+            ->addMarker(new Marker(
+                position: new Point(50.511097, 3.589901),
+                title: 'Royale Fanfare Communale de Péruwelz'
+            ))
+            ->addMarker(new Marker(
+                position: new Point(50.449078, 3.815871),
+                title: 'Harmonie Royale de Saint-Ghislain'
+            ))
+            ->addMarker(new Marker(
+                position: new Point(50.447907, 3.591055),
+                title: 'Harmonie Municipale de Condé-sur-l\'Escaut'
+            ))
+            ->addMarker(new Marker(
+                position: new Point(50.426841, 3.866219),
+                title: 'La Bouverie',
+                extra: ['icon_mask_url' => 'images/logo-gais-carabiniers-bernissart.webp']
+//                extra: ['icon_mask_url' => 'https://maps.gstatic.com/mapfiles/place_api/icons/v2/tree_pinlet.svg']
+            ))
+        ;
+
+        return $this->render('club/contact.html.twig', [
+            'myMap' => $myMap
         ]);
     }
 }

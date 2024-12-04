c32b9b4bfba1d51f6e67b80bf40071ef13777524
Novitz Jean-Philippe <novitz@gmail.com>
2024-12-04 03:51:30 +0100

    Add turbo views for transition
    ** fixed menu that doesn't work after first page change, add flowbite re-init **
    
    - add turbo-view-transitions
    - add js code in asset.js
    - add meta  'view-transition' in base

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,46 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import 'flowbite';
+// import 'flowbite';
+import * as Turbo from '@hotwired/turbo';
+import './bootstrap.js';
 import './styles/app.css';
+import { shouldPerformTransition, performTransition } from 'turbo-view-transitions';
+import { initFlowbite } from 'flowbite';
+// import 'flowbite';
 
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+document.addEventListener('turbo:before-render', (event) => {
+    if (shouldPerformTransition()) {
+        event.preventDefault();
+        performTransition(document.body, event.detail.newBody, async () => {
+            await event.detail.resume();
+        });
+    }
+});
+document.addEventListener('turbo:load', () => {
+    // View Transitions don't play nicely with Turbo cache
+    if (shouldPerformTransition()) Turbo.cache.exemptPageFromCache();
+});
+
+document.addEventListener('turbo:before-frame-render', (event) => {
+    if (shouldPerformTransition()) {
+        event.preventDefault();
+        performTransition(event.target, event.detail.newFrame, async () => {
+            await event.detail.resume();
+        });
+    }
+});
+document.addEventListener('turbo:render', () => {
+    initFlowbite();
+});
+document.addEventListener('turbo:frame-render', () => {
+    initFlowbite();
+});

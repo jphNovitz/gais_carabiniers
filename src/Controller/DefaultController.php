2824b152cad4f99788ddd8d17023e9ab0c4db7f5
Novitz Jean-Philippe <novitz@gmail.com>
2024-12-05 05:46:09 +0100

    Refactor  DefaultController
    change cache TTL

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,15 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\UX\Map\Map;
-use Symfony\UX\Map\Point;
-use Symfony\UX\Map\Marker;
-use Symfony\UX\Map\InfoWindow;
+
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    #[Cache(maxage: 8640, public: true, mustRevalidate: true)]
+    #[Cache(maxage: 31536000, public: true, mustRevalidate: true)]
     public function index(ClubRepository $clubRepository): Response
     {
 

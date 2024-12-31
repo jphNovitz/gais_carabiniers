2d3d047ee5efc1933ae513ac7e1be14c9432fd1b
Novitz Jean-Philippe <novitz@gmail.com>
2024-12-31 05:35:26 +0100

    Add facebook event partial, add it to landin/index
    Add repository function

--- src/Repository/FacebookEventRepository.php
+++ src/Repository/FacebookEventRepository.php
@@ -16,5 +16,14 @@ class FacebookEventRepository extends ServiceEntityRepository
         parent::__construct($registry, FacebookEvent::class);
     }
 
-  
+    public function findLastFutureElement(): ?FacebookEvent
+    {
+        return $this->createQueryBuilder('f')
+            ->where('f.date > :now')      // Filtrer les dates dans le futur
+            ->setParameter('now', new \DateTime()) // Définir le paramètre actuel
+            ->orderBy('f.date', 'ASC')    // Trier par date ascendante (le plus proche du futur en premier)
+            ->setMaxResults(1)            // Limiter à 1 résultat
+            ->getQuery()
+            ->getOneOrNullResult();       // Retourner l'entité ou null si aucun résultat
+    }
 }

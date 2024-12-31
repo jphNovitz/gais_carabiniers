1405f56a3dd9347cb210bfaeea51882109d1b6ec
Novitz Jean-Philippe <novitz@gmail.com>
2024-12-31 05:36:49 +0100

    refactor FacebookEvent formType
    Add labels for translation

--- src/Form/FacebookEventType.php
+++ src/Form/FacebookEventType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Dto\FacebookEventDto;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,11 +14,16 @@ class FacebookEventType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'event.name',
+            ])
             ->add('date', DateType::class, [
                 'widget' => 'single_text',
+                'label' => 'event.date',
             ])
-            ->add('facebookLink')
+            ->add('facebookLink', TextType::class, [
+                'label' => 'event.link',
+            ])
             
         ;
     }
@@ -26,6 +32,7 @@ class FacebookEventType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => FacebookEventDto::class,
+            'translation_domain' => 'messages',
         ]);
     }
 }

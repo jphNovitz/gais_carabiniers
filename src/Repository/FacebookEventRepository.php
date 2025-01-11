619490e4e6dc19da5bff15f2587f32e1db972149
Novitz Jean-Philippe <novitz@gmail.com>
2025-01-11 06:22:34 +0100

    Refactor (landing) facebook event
    - refactor twig partial
    - Repository: change order -> DESC

--- src/Repository/FacebookEventRepository.php
+++ src/Repository/FacebookEventRepository.php
@@ -21,7 +21,7 @@ class FacebookEventRepository extends ServiceEntityRepository
         return $this->createQueryBuilder('f')
             ->where('f.date > :now')      // Filtrer les dates dans le futur
             ->setParameter('now', new \DateTime()) // Définir le paramètre actuel
-            ->orderBy('f.date', 'ASC')    // Trier par date ascendante (le plus proche du futur en premier)
+            ->orderBy('f.date', 'DESC')   
             ->setMaxResults($limit)            // Limiter à 1 résultat
             ->getQuery()
             ->getResult();       // Retourner l'entité ou null si aucun résultat

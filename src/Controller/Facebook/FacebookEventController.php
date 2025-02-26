be8bf1ee110a447b62f6fec7460084df967e0281
Novitz Jean-Philippe <novitz@gmail.com>
2025-02-26 02:28:03 +0100

    Add Facebook Controller/template
    adq twig/intl package

---
+++ src/Controller/Facebook/FacebookEventController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Facebook;
+
+use App\Repository\FacebookEventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FacebookEventController extends AbstractController
+{
+    #[Route('/agenda', name: 'app_agenda')]
+    public function agenda(FacebookEventRepository $facebookEventRepository): Response
+    {
+
+        return $this->render('facebook/index.html.twig', [
+            'facebookEvents' => $facebookEventRepository->findAllFutureElements(),
+            'facebookEventsPast' => $facebookEventRepository->findAllPastElements()
+        ]);
+    }
+}

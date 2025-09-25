e2e9f2b841a15b6e4398b662dd66dd2f1451b524
Novitz Jean-Philippe <novitz@gmail.com>
2025-09-25 04:50:52 +0200

    Add mappers for Meeting and MeetingParticipant entities

---
+++ src/Contract/MeetingCreatorInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Contract;
+
+interface MeetingCreatorInterface
+{
+    public function createMeeting(): void;
+}
---
+++ src/Service/MeetingCreator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Service;
+
+use App\Contract\MeetingCreatorInterface;
+use App\Entity\Meeting;
+use Doctrine\ORM\EntityManagerInterface;
+
+class MeetingCreator implements MeetingCreatorInterface
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+    }
+
+    public function createMeeting(): void
+    {
+        $months = [
+            '1' => 'janvier',
+            '2' => 'février',
+            '3' => 'mars',
+            '4' => 'avril',
+            '5' => 'mai',
+            '6' => 'juin',
+            '7' => 'juillet',
+            '8' => 'août',
+            '9' => 'septembre',
+            '10' => 'octobre',
+            '11' => 'novembre',
+            '12' => 'décembre',
+        ];
+
+        $now = new \DateTimeImmutable();
+        $monthNumber = (int) $now->format('n'); // 1-12 without leading zeros
+        $monthName = $months[(string)$monthNumber] ?? '';
+
+        $meeting = new Meeting();
+        $meeting->setDate($now);
+        $meeting->setStatus('start');
+        $meeting->setLabel('Tir du mois de ' . $months[(new \DateTime())->format('n')]);
+        $meeting->setDate(new \DateTimeImmutable());
+        $meeting->setOpenedAt(new \DateTimeImmutable());
+
+        $this->em->persist($meeting);
+        $this->em->flush();
+    }
+}

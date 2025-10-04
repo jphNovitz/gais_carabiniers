cd019a65395cc8f4711328f31163f4e2974dfdb1
Novitz Jean-Philippe <novitz@gmail.com>
2025-10-04 05:41:36 +0200

    Add participant management functionality to MeetingController and templates

--- src/Controller/Admin/Meeting/MeetingController.php
+++ src/Controller/Admin/Meeting/MeetingController.php
@@ -3,8 +3,16 @@
 namespace App\Controller\Admin\Meeting;
 
 use App\Contract\MeetingCreatorInterface;
+use App\Contract\MeetingParticipantAdderInterface;
+use App\Entity\Meeting;
+use App\Entity\MeetingParticipant;
+use App\Form\AddParticipantsType;
+use App\Form\MeetingParticipantType;
+use App\Mapper\MeetingMapper;
+use App\Mapper\MeetingParticipantMapper;
 use App\Repository\MeetingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -22,8 +30,43 @@ final class MeetingController extends AbstractController
     #[Route('/create', name: 'admin_meeting_create', methods: ['POST'])]
     public function create(MeetingCreatorInterface $meetingCreator): Response
     {
-        $meetingCreator->createMeeting();
-        die;
-        return $this->redirectToRoute('admin_meeting_index', [], Response::HTTP_FOUND);
+        $meetingId = $meetingCreator->createMeeting();
+
+        return $this->redirectToRoute('admin_meeting_add_participant',
+            ['id' => $meetingId],
+            Response::HTTP_SEE_OTHER);
+    }
+
+    #[route('/{id}/add-participants', name: 'admin_meeting_add_participant', methods: ['GET', 'POST'])]
+    public function update(Meeting $meeting, Request $request, MeetingParticipantAdderInterface $participantAdder): Response
+    {
+        $form = $this->createForm(AddParticipantsType::class, null, ['meeting' => $meeting]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $members = $form->get('shooters')->getData();
+            if ($participantAdder->addMany($meeting, $members) > 0) {
+                $this->addFlash('success', 'Participants added successfully.');
+                return $this->redirectToRoute('admin_meeting_show', ['id' => $meeting->getId()], Response::HTTP_SEE_OTHER);
+            } else {
+                $this->addFlash('warning', 'No new participants were added.');
+                return $this->redirectToRoute('admin_meeting_add_participant', ['id' => $meeting->getId()], Response::HTTP_SEE_OTHER);
+            }
+
+        }
+
+        return $this->render('admin/meeting/participant.html.twig', [
+            'meeting' => $meeting,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_meeting_show', methods: ['GET'])]
+    public function show(Meeting $meeting): Response
+    {
+        $dto = MeetingMapper::fromEntity($meeting);
+        return $this->render('admin/meeting/show.html.twig', [
+            'meeting' => $dto,
+        ]);
     }
 }

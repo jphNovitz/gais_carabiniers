ac1f731e2dafcd636ea0be6a121686cab72c96a4
Novitz Jean-Philippe <novitz@gmail.com>
2026-02-04 13:19:00 +0100

    feat (landing): add bento section with event timer and snapshot display

---
+++ assets/controllers/event_timer_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = ['output'];
+    static values = {
+        toto: String,
+        startTime: String,
+    };
+
+    connect() {
+
+            console.log('event-timer CONNECT', this.element);
+
+        const startTime = new Date(this.startTimeValue);
+        this.updateTimer(startTime);
+
+        this.timerInterval = setInterval(() => this.updateTimer(startTime), 1000);
+    }
+
+    disconnect() {
+        clearInterval(this.timerInterval);
+    }
+
+    updateTimer(startTime) {
+        const now = new Date();
+        let diff = Math.floor((now - startTime) / 1000);
+        diff = Math.abs(diff);
+
+        const days = Math.floor(diff / 86400);
+        diff -= days * 86400;
+        const hours = Math.floor(diff / 3600);
+        diff -= hours * 3600;
+        const minutes = Math.floor(diff / 60);
+        const seconds = diff % 60;
+
+        this.outputTarget.textContent = `Dans ${days} jour(s) ${hours}h ${minutes}m ${seconds}s`;
+    }
+}

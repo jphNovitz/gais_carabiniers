38f2161edc873058d91f9b3df97f00deaf49b2db
Novitz Jean-Philippe <novitz@gmail.com>
2026-02-04 13:18:08 +0100

    feat (controller): update index method to include MeetingSnapshot repository and adjust Facebook events retrieval

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ClubRepository;
 use App\Repository\FacebookEventRepository;
+use App\Repository\MeetingSnapshotRepository;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,15 +16,20 @@ class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
     #[Cache(maxage: 31536000, public: true, mustRevalidate: true)]
-    public function index(ClubRepository $clubRepository, FacebookEventRepository $facebookEventRepository, PostRepository $postRepository): Response
+    public function index(ClubRepository $clubRepository,
+                          FacebookEventRepository $facebookEventRepository,
+                          PostRepository $postRepository,
+                          MeetingSnapshotRepository $meetingSnapshotRepository): Response
     {
         $edito = $postRepository->findHomeEdito('Edito');
-        $facebookEvents = $facebookEventRepository->findLastFutureElements(3);
+        $facebookEvents = $facebookEventRepository->findLastFutureElements(1);
+        $snapshot = $meetingSnapshotRepository->findSeasonStandings(date('Y'));
 
 
         return $this->render('landing/index.html.twig', [
             'edito' => $edito,
-            'facebookEvents' => $facebookEvents,
+            'facebookEvent' => $facebookEvents[0] ?? null,
+            'snapshot' => $snapshot,
         ]);
     }
 }

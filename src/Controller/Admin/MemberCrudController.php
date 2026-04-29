1f432516cb7e05d9da6fac9e2710c0c6344c1f8c
Novitz Jean-Philippe <novitz@gmail.com>
2026-04-29 13:42:34 +0200

    feat(member) update member crud controller
    - add fields in member controller
    - add translation keys
    - remove old member controller (custom)
    - remover old member admin templates (custom/basic)

--- src/Controller/Admin/MemberCrudController.php
+++ src/Controller/Admin/MemberCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Member;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +16,16 @@ class MemberCrudController extends AbstractCrudController
         return Member::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
-    }
-    */
+         return [
+            TextField::new('firstName')->setLabel('member.firstname')->setColumns(6),
+            TextField::new('lastName')->setLabel('member.lastname')->setColumns(6),
+            BooleanField::new('isactive')->setLabel('member.isActive')->setColumns(4),
+            BooleanField::new('usesSupport')->setLabel('member.usesSupport')->setColumns(4),
+                 
+       ];
+     }
+     
 }

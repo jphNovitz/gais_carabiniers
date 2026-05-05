589bbef40907543aa3b41632626fa99343e20b5f
Novitz Jean-Philippe <novitz@gmail.com>
2026-05-05 02:24:24 +0200

    feat(ClubMembership) add migration files

---
+++ migrations/Version20260504224525.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260504224525 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE club ADD is_home TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE year_snapshot CHANGE average_hits average_hits NUMERIC(6, 2) DEFAULT \'0\' NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE club DROP is_home');
+        $this->addSql('ALTER TABLE year_snapshot CHANGE average_hits average_hits NUMERIC(6, 2) DEFAULT \'0.00\' NOT NULL');
+    }
+}

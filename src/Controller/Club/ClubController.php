02155d8f68f29e67f55003e9e23738bd0db482ca
Novitz Jean-Philippe <novitz@gmail.com>
2026-07-23 14:49:51 +0200

    Redesign contact detail page

--- src/Controller/Club/ClubController.php
+++ src/Controller/Club/ClubController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller\Club;
 
-use App\Repository\ClubRepository;
-use App\Repository\FacebookEventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,6 +12,9 @@ use Symfony\UX\Map\InfoWindow;
 
 class ClubController extends AbstractController
 {
+    private const CLUB_LATITUDE = 50.47707987702586;
+    private const CLUB_LONGITUDE = 3.6501962312217797;
+
     #[Route('/notre-histoire', name: 'app_about')]
     public function about(): Response
     {
@@ -22,48 +23,30 @@ class ClubController extends AbstractController
     }
 
     #[Route('/contact', name: 'app_contact')]
-    public function contact(ClubRepository $clubRepository): Response
+    public function contact(): Response
     {
-        $myMap = (new Map());
-        $myMap
-            // Explicitly set the center and zoom
-            ->center(new Point(50.472738, 3.661204))
-//            ->zoom(6)
+        $clubLocation = new Point(self::CLUB_LATITUDE, self::CLUB_LONGITUDE);
 
-            // Or automatically fit the bounds to the markers
+        $myMap = (new Map())
             ->fitBoundsToMarkers()
             ->addMarker(new Marker(
-                position: new Point(50.472738, 3.661204),
-//                title: 'Les Gais Carabiniers de Bernissart',
+                position: $clubLocation,
+                title: 'Les Gais Carabiniers de Bernissart',
                 infoWindow: new InfoWindow(
                     headerContent: '<h3>Les Gais Carabiniers de Bernissart</h3>',
                     content: '<p>Rue Lotard 16, 7320 Bernissart</p>'
-                ),
-
-//                icon: 'images/logo-gais-carabiniers-bernissart.webp'
-            ))
-            ->addMarker(new Marker(
-                position: new Point(50.511097, 3.589901),
-                title: 'Royale Fanfare Communale de Péruwelz'
-            ))
-            ->addMarker(new Marker(
-                position: new Point(50.449078, 3.815871),
-                title: 'Harmonie Royale de Saint-Ghislain'
-            ))
-            ->addMarker(new Marker(
-                position: new Point(50.447907, 3.591055),
-                title: 'Harmonie Municipale de Condé-sur-l\'Escaut'
-            ))
-            ->addMarker(new Marker(
-                position: new Point(50.426841, 3.866219),
-                title: 'La Bouverie',
-                extra: ['icon_mask_url' => 'images/logo-gais-carabiniers-bernissart.webp']
-//                extra: ['icon_mask_url' => 'https://maps.gstatic.com/mapfiles/place_api/icons/v2/tree_pinlet.svg']
-            ))
-        ;
+                )
+            ));
 
         return $this->render('club/contact.html.twig', [
-            'myMap' => $myMap
+            'myMap' => $myMap,
+            'mapDirectionsUrl' => sprintf(
+                'https://www.google.com/maps/search/?api=1&query=%.14F,%.16F',
+                self::CLUB_LATITUDE,
+                self::CLUB_LONGITUDE,
+            ),
+            'latitude' => self::CLUB_LATITUDE,
+            'longitude' => self::CLUB_LONGITUDE,
         ]);
     }
 

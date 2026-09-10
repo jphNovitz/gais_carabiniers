a927443d9df0746222f9116d2910d66c345747b1
Novitz Jean-Philippe <novitz@gmail.com>
2026-09-10 04:36:11 +0200

    refactor: update frontend runtime configuration

--- importmap.php
+++ importmap.php
@@ -26,9 +26,6 @@ return [
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
-    ],
     'flowbite' => [
         'version' => '2.5.2',
     ],
@@ -57,6 +54,9 @@ return [
     ],
     '@symfony/ux-live-component' => [
         'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '8.0.23',
     ],
     'daisyui' => [
         'version' => '5.5.23',

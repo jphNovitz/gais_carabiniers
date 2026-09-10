49c2fd27ae20baa10810cbfda0521616b85d0ec2
Novitz Jean-Philippe <novitz@gmail.com>
2026-09-10 05:42:42 +0200

    fix: restore mobile navigation menu

--- tests/Controller/NavigationControllerTest.php
+++ tests/Controller/NavigationControllerTest.php
@@ -10,6 +10,32 @@ use Twig\Environment;
 
 final class NavigationControllerTest extends WebTestCase
 {
+    public function testMobileMenuWorksWithoutJavaScript(): void
+    {
+        self::bootKernel();
+
+        $request = Request::create('/faq');
+        $request->attributes->set('_route', 'app_faq');
+
+        /** @var RequestStack $requestStack */
+        $requestStack = static::getContainer()->get(RequestStack::class);
+        $requestStack->push($request);
+
+        try {
+            /** @var Environment $twig */
+            $twig = static::getContainer()->get(Environment::class);
+            $crawler = new Crawler($twig->render('_header/_header.html.twig', [
+                'globalInfos' => null,
+            ]));
+        } finally {
+            $requestStack->pop();
+        }
+
+        self::assertCount(1, $crawler->filter('details#mobile-main-menu > summary'));
+        self::assertCount(1, $crawler->filter('details#mobile-main-menu > #mobile-menu-2'));
+        self::assertCount(0, $crawler->filter('[data-collapse-toggle="mobile-menu-2"]'));
+    }
+
     public function testCompetitionsMenuWorksWithoutJavaScript(): void
     {
         self::bootKernel();
